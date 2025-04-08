7bc8756e7e86568d50981eec74728611b5b73fdf
Patryk Rejent <patrykrejent@wp.pl>
2025-04-08 21:13:17 +0200

    Create a feature for TaskService.java. and a new funtion to Task.java it is setDone()

--- src/main/java/eu/tutorials/Task.java
+++ src/main/java/eu/tutorials/Task.java
@@ -16,6 +16,10 @@ public class Task {
     public boolean isDone() {
         return done;
     }
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
 
     @Override
     public String toString() {
--- src/main/java/eu/tutorials/TaskService.java
+++ src/main/java/eu/tutorials/TaskService.java
@@ -1,4 +1,34 @@
 package eu.tutorials;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TaskService {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(String name) {
+        tasks.add(new Task(name));
+        System.out.println("Task added: " + name);
+    }
+
+    public void markTaskAsDone(String name) {
+        for (Task task : tasks) {
+            if (task.getName().equals(name)) {
+                task.setDone(true);
+                System.out.println("Task marked as done: " + name);
+                return;
+            }
+        }
+        System.out.println("Task not found: " + name);
+    }
+    public void listTasks() {
+        System.out.println("----- Current tasks -----");
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
+        System.out.println("-------------------------");
+    }
 }
