3cd91563a112bb48603ddff2480dfa4fdf2a37ee
Patryk Rejent <patrykrejent@wp.pl>
2025-04-08 21:02:54 +0200

    eliminate my mistake

--- src/main/java/eu/tutorials/Task.java
+++ src/main/java/eu/tutorials/Task.java
@@ -1,27 +1,5 @@
 package eu.tutorials;
 
 public class Task {
-    private final String name;
-    private boolean done;
 
-    public Task(String name) {
-        this.name = name;
-        this.done = false;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "name='" + name + '\'' +
-                ", done=" + done +
-                '}';
-    }
 }
