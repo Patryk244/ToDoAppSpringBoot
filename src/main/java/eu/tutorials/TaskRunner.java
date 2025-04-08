fb7f5eba728d749438a605e065f14536d202cd27
Patryk Rejent <patrykrejent@wp.pl>
2025-04-08 21:17:33 +0200

    My program includes all function

--- src/main/java/eu/tutorials/TaskRunner.java
+++ src/main/java/eu/tutorials/TaskRunner.java
@@ -1,4 +1,35 @@
 package eu.tutorials;
 
-public class TaskRunner {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskRunner implements CommandLineRunner {
+
+    private final TaskService taskService;
+
+    public TaskRunner(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("=== Starting TaskRunner ===");
+
+        // Dodajemy kilka zadań
+        taskService.addTask("Buy milk");
+        taskService.addTask("Finish Spring tutorial");
+        taskService.addTask("Walk the dog");
+
+        // Wyświetlamy listę
+        taskService.listTasks();
+
+        // Oznaczamy "Buy milk" jako zrobione
+        taskService.markTaskAsDone("Buy milk");
+
+        // Ponownie wyświetlamy listę
+        taskService.listTasks();
+
+        System.out.println("=== TaskRunner finished ===");
+    }
 }
